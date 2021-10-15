0b64442b9631e897e75dc2b96b281bdbcb67590b
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-10-15 10:07:18 -0300

    start to build api endpoints testing django urls

---
+++ backend/base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+
+
+# Create your views here.
+def get_routes(request):
+    routes = [
+        '/api/products/', '/api/products/create/', '/api/products/upload/',
+        '/api/products/<id>/reviews/', '/api/products/top/',
+        '/api/products/<id>/', '/api/products/delete/<id>',
+        '/api/products/<update>/<id>'
+    ]
+    return JsonResponse(routes, safe=False)
+
+
+def get_products(request):
+    return JsonResponse(products, safe=False)
